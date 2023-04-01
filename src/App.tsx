4b8435730f5d788a0c868a39eb50962c4f91fe9f
yahia <mohamedyahia207545>
2023-04-01 23:22:59 +0200

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Home from "./components/Home";
 import { Iitems } from "./types/Itypes";
 import Login from "./components/Login";
 import useMenu from "./hooks/useMenu";
-import { ThemeProvider } from "./context/ThemeContext";
+// import { ThemeProvider } from "./context/ThemeContext";
 
 let pageSize = 3;
 
@@ -76,51 +76,51 @@ const App: FC<Iitems> = ({}) => {
 
   return (
     <div className="w-[786px] m-auto">
-      <ThemeProvider childern={undefined}>
-        <BrowserRouter>
-          <Header
-            itemsInCart={items.filter((item: Iitems) => item.inCart).length}
+      {/* <ThemeProvider childern={undefined}> */}
+      <BrowserRouter>
+        <Header
+          itemsInCart={items.filter((item: Iitems) => item.inCart).length}
+        />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                counters={items.filter((item) => item.inCart)}
+                handleIncrement={handleIncrement}
+                handleDecrement={handleDecrement}
+                handleDelete={handleDelete}
+                handelReset={handelReset}
+              />
+            }
           />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/cart"
-              element={
-                <Cart
-                  counters={items.filter((item) => item.inCart)}
-                  handleIncrement={handleIncrement}
-                  handleDecrement={handleDecrement}
-                  handleDelete={handleDelete}
-                  handelReset={handelReset}
-                />
-              }
-            />
-            <Route path="/about">
-              <Route index element={<About />} />
-            </Route>
-            <Route
-              path="/menu"
-              element={
-                <Menu
-                  items={items}
-                  categories={categories}
-                  pageSize={pageSize}
-                  currentCategory={currentCategory}
-                  currentPage={currentPage}
-                  noOfPage={noOfPage}
-                  addToCartHandle={addToCartHandle}
-                  changeCurrentCategory={changeCurrentCategory}
-                  changeCurrentPage={changeCurrentPage}
-                />
-              }
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/product/:productid/:country?" element={<Product />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </BrowserRouter>
-        {/* <ITI track="UI" /> */}
-      </ThemeProvider>
+          <Route path="/about">
+            <Route index element={<About />} />
+          </Route>
+          <Route
+            path="/menu"
+            element={
+              <Menu
+                items={items}
+                categories={categories}
+                pageSize={pageSize}
+                currentCategory={currentCategory}
+                currentPage={currentPage}
+                noOfPage={noOfPage}
+                addToCartHandle={addToCartHandle}
+                changeCurrentCategory={changeCurrentCategory}
+                changeCurrentPage={changeCurrentPage}
+              />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/product/:productid/:country?" element={<Product />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <ITI track="UI" /> */}
+      {/* </ThemeProvider> */}
     </div>
   );
 };
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,12 +1,11 @@
 import { createContext } from "react";
-import { useState  } from "react";
+import { useState } from "react";
 
 const ThemeContext = createContext({});
 
-
-export function ThemeProvider({childern}){
-  const [theme, setTheme] = useState("light");
-  return (<ThemeContext.Provider value={{ theme, setTheme }}>{childern}</ThemeContext.Provider>)
-}
+// export function ThemeProvider({childern}){
+//   const [theme, setTheme] = useState("light");
+//   return (<ThemeContext.Provider value={{ theme, setTheme }}>{childern}</ThemeContext.Provider>)
+// }
 
 export default ThemeContext;
